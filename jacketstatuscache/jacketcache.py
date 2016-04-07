1338878afaeeb1861f581f6f74c982a01ae4f78f
back1860 <back1860@outlook.com>
2016-04-07 19:00:25 +0800

    1. when can not find record in cache, should not try to reflash the cache, return None directly.

--- jacketstatuscache/jacketcache.py
+++ jacketstatuscache/jacketcache.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 
 import log as logger
-import power_state
 import threading
 import time
 
@@ -15,16 +14,6 @@ logger.init("jacket-cache", output=False)
 _cache_lock = threading.Lock()
 
 
-def singleton(cls, *args, **kw):
-    instances = {}
-    def _singleton():
-        if cls not in instances:
-            instances[cls] = cls(*args, **kw)
-        return instances[cls]
-    return _singleton
-
-
-@singleton()
 class JacketStatusCache(object):
 
     def __init__(self, synchronizer):
@@ -34,8 +23,8 @@ class JacketStatusCache(object):
         self.last_sync_time = time.time()
 
     def query_status(self, instance_id):
-        _cache_lock.acquire()
         now = time.time()
+        _cache_lock.acquire()
         try:
             if now - self.last_sync_time > EXP_TIME:
                 logger.info("cache have expire. sync cache. now = %s, last sync time = %s" % (now, self.last_sync_time))
@@ -53,22 +42,4 @@ class JacketStatusCache(object):
 
         logger.debug("query status, can not find instance record, instance_id = %s." % instance_id)
 
-        _cache_lock.acquire()
-        try:
-            logger.debug("query status, synchronize status.")
-            self.status_map = self.synchronizer.synchronize_status()
-            self.last_sync_time = time.time()
-            logger.info("sync cache success, cache size = %s." % len(self.status_map))
-        except Exception as e:
-            logger.error("sync cache failed.", e)
-        finally:
-            _cache_lock.release()
-
-        logger.debug("query status, try to find instance record after synchronize, instance_id = %s." % instance_id)
-        if instance_id in self.status_map:
-            status = self.status_map.get(instance_id)
-            logger.debug("query status success, instance_id = %s, status = %s" % (instance_id, status))
-            return status
-
-        logger.error("can not get instance status, instance_id: %s" % instance_id)
-        return power_state.NOSTATE
+        return None
